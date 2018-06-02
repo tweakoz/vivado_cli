6ec4bc10f16566a17f5a546a47dc842031c149e9
Michael Mayers <michael@tweakoz.com>
2018-06-01 21:25:48 -0700

    add commandline helper config

---
+++ hls_config.py
@@ -0,0 +1,10 @@
+#
+project_name = "p2"
+top_level_function_name = "trackertest"
+src_dir_name = "src"
+tb_dir_name = "tb"
+src_files = ["p2.v"]
+part_name = "xc7a35tcpg236-1"
+clock_period = "10"
+language="verilog"
+
